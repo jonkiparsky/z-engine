8f9578745e40d6f49e00f71851bdcffab4685751
jon.kiparsky <jon.kiparsky@10154fc8-2dca-eee6-113c-c23c446b7b7e>
2011-08-20 04:42:54 +0000

    Added some framework towards logging, in a utilities class. Also some javadoc for Sentence.java

--- src/zengine/Fixture.java
+++ src/zengine/Fixture.java
@@ -1,12 +1,14 @@
 package zengine;
 
-
 /**
 *	Stub, to build out: represents immobile nouns. 
 */
 public class Fixture extends Noun
 {
 
-
+	public Fixture(String name)
+	{
+		super(name);
+	}
 
 }
---
+++ src/zengine/Utils.java
@@ -0,0 +1,23 @@
+package zengine;
+
+/**
+* A collection of useful methods 
+*/
+public class Utils
+{
+	private static enum DebugLevel
+	{
+
+		// working on this - set log levels from within the program, get debug
+		// info directly to the console while testing, or whatever
+		NIL, LOG_SIMPLE, LOG_COMPLETE, CONSOLE
+	}
+
+	/**
+	* Print a debug message to a destination. Currently, print to screen.
+	*/
+	protected static void debug(String s)
+	{
+		System.out.println(s);
+	}
+}
