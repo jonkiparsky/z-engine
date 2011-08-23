90c0dee6bc6c29cd6f64a487a424b9c9e45fec26
jon.kiparsky <jon.kiparsky@10154fc8-2dca-eee6-113c-c23c446b7b7e>
2011-08-23 05:13:47 +0000

    Mechanism for confirming actions. Framework, needs a little fit and finish.

--- src/gamefiles/grammar/Quit.java
+++ src/gamefiles/grammar/Quit.java
@@ -2,8 +2,9 @@ package gamefiles.grammar;
 
 import zengine.Verb;
 import zengine.ZEngineMain;
+import zengine.Confirmable;
 
-public class Quit extends Verb {
+public class Quit extends Verb implements Confirmable{
     
         public static String desc;
         static
@@ -18,6 +19,24 @@ public class Quit extends Verb {
     
         public void execute()
         {
-                ZEngineMain.state.quit();
+				System.out.println("Are you sure you want to quit?");
+				ZEngineMain.state.setConfirm((Confirmable)this);
         }
+		
+			public void confirm(String s)	
+		{
+			if (s.equals("YES"))
+			{
+				String quitString = "Be seeing you...";
+                ZEngineMain.state.quit(quitString);
+			}
+
+			else if (s.equals("NO"))
+			{
+				System.out.println("So you'll stick around. Excellent.");
+				ZEngineMain.state.clearConfirm();
+			}
+		}
+
+
 }
