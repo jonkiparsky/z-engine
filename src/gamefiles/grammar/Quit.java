ac81e431473b6e7fc9a1f7f46e4de0da67776276
i.Snowblind7 <i.Snowblind7@gmail.com>
2011-08-25 01:23:24 +0000

    Implemented slight fix for answering Confirm Quit after making other commands. Implementation needs much improvement, but it's a start.

--- src/gamefiles/grammar/Quit.java
+++ src/gamefiles/grammar/Quit.java
@@ -3,10 +3,13 @@ package gamefiles.grammar;
 import zengine.Verb;
 import zengine.ZEngineMain;
 import zengine.Confirmable;
+import zengine.Timer;
 
 public class Quit extends Verb implements Confirmable{
     
         public static String desc;
+        private static float timeAsked;
+        private static float timeElapsed;
         static
         {
                 desc = "- Quit\nUsage: Quit\nDescription: Quits Game";
@@ -19,24 +22,29 @@ public class Quit extends Verb implements Confirmable{
     
         public void execute()
         {
-				System.out.println("Are you sure you want to quit?");
-				ZEngineMain.state.setConfirm((Confirmable)this);
+		System.out.println("Are you sure you want to quit?");
+		ZEngineMain.state.setConfirm((Confirmable)this);
+                timeAsked = Timer.getTime();
         }
 		
-			public void confirm(String s)	
+        public void confirm(String s)	
+        {
+                timeElapsed = Timer.getTime() - timeAsked;
+                if (timeElapsed > 1)
+                {
+                        System.out.println("You have taken too long to confirm quitting.");
+                }
+                else if (s.equals("YES"))
 		{
-			if (s.equals("YES"))
-			{
-				String quitString = "Be seeing you...";
-                ZEngineMain.state.quit(quitString);
-			}
-
-			else if (s.equals("NO"))
-			{
-				System.out.println("So you'll stick around. Excellent.");
-				ZEngineMain.state.clearConfirm();
-			}
+			String quitString = "Be seeing you...";
+                        ZEngineMain.state.quit(quitString);
 		}
+		else if (s.equals("NO"))
+		{
+			System.out.println("So you'll stick around. Excellent.");
+			ZEngineMain.state.clearConfirm();
+		}
+	}
 
 
 }
