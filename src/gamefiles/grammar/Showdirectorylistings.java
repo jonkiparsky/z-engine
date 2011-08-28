80a34ebdf8c12c44c90162a0924b9930c74c70b9
jon.kiparsky <jon.kiparsky@10154fc8-2dca-eee6-113c-c23c446b7b7e>
2011-08-28 19:57:14 +0000

    doh

---
+++ src/gamefiles/grammar/Showdirectorylistings.java
@@ -0,0 +1,23 @@
+package gamefiles.grammar;
+
+import zengine.Verb;
+import zengine.ZEngineMain;
+import zengine.Utils;
+import zengine.Confirmable;
+import zengine.Timer;
+
+public class Showdirectorylistings extends Verb{
+    
+        public Showdirectorylistings()
+        {
+                super("QUIT");
+                acceptable.add(None.class);
+        }
+    
+        public void execute()
+        {
+				Utils.printDirectoryListings();
+        }
+
+
+}
